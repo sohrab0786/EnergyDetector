77e96d896fc5b180390174ec396795139215ecf0
sohrab0786 <sohrab.emam@aitoxr.com>
2025-04-09 16:29:12 +0530

    Update settings.py

--- CoolRoof/settings.py
+++ CoolRoof/settings.py
@@ -89,14 +89,13 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-#STATIC_ROOT = ""
+STATIC_ROOT = ""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'CoolRoof/static'),
     os.path.join(BASE_DIR, 'static'),
    
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Email settings
