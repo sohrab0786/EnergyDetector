a2bdaf0c678bdac89597767d622e1f1586e96396
sohrab0786 <sohrab.emam@aitoxr.com>
2025-04-10 13:12:30 +0530

    Update settings.py

--- CoolRoof/settings.py
+++ CoolRoof/settings.py
@@ -95,8 +95,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
  
     os.path.join(BASE_DIR, 'static'),
-
-   
+    os.path.join(BASE_DIR, 'CoolRoof/static'),
+    os.path.join(BASE_DIR, 'templates/static),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
